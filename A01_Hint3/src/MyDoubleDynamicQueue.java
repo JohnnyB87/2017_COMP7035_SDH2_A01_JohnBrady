44a38b51d0ef8e082810d7092e27db00a6cc5b97
JohnnyB87 <johnbrady1987@gmail.com>
2017-10-19 23:44:47 +0100

    Implemented basic operations, 1 line methods

--- A01_Hint3/src/MyDoubleDynamicQueue.java
+++ A01_Hint3/src/MyDoubleDynamicQueue.java
@@ -4,34 +4,48 @@ public class MyDoubleDynamicQueue<T> implements MyQueue<T> {
 	//--------------------------------------------------
 	// Attributes
 	//--------------------------------------------------
-
+	MyDoubleLinkedNode<T> head;
+	MyDoubleLinkedNode<T> tail;
+	int numItems;
+	int tailItems;
+	int headItems;
 	
 	//-------------------------------------------------------------------
 	// Basic Operation --> Check if myQueue is empty: myCreateEmpty
 	//-------------------------------------------------------------------	
 	public MyDoubleDynamicQueue(){
-
+		this.numItems = 0;
+		this.head = null;
+		this.tail = null;
 	}
 
 	//-------------------------------------------------------------------
 	// Basic Operation --> Check if myQueue is empty: isEmpty
 	//-------------------------------------------------------------------	
 	public boolean isEmpty(){
-
+		return this.numItems == 0;
 	}
 	
 	//-------------------------------------------------------------------
 	// Basic Operation (Partial) --> Get first element from front of MyQueue: first
 	//-------------------------------------------------------------------
 	public T first(){
-
+		return this.head.getInfo();
 	}
 
 	//-------------------------------------------------------------------
 	// Basic Operation --> Add element to back of MyQueue: addByFirst 
 	//-------------------------------------------------------------------
 	public void addByFirst(T element){
+		if(this.head == null)
+			this.head = new MyDoubleLinkedNode<T>(null, element,this.tail);
+		else {
+			MyDoubleLinkedNode<T> currentNode = this.head;
+			this.head = new MyDoubleLinkedNode<T>(null,element,currentNode);
+			currentNode.setLeft(this.head);
 
+		}
+		this.numItems++;
 	}
 	
 	//-------------------------------------------------------------------
@@ -45,7 +59,7 @@ public class MyDoubleDynamicQueue<T> implements MyQueue<T> {
 	// Basic Operation (Partial) --> Get first element from front of MyQueue: last
 	//-------------------------------------------------------------------
 	public T last(){
-
+		return this.tail.getInfo();
 	}
 
 	//-------------------------------------------------------------------
@@ -61,5 +75,9 @@ public class MyDoubleDynamicQueue<T> implements MyQueue<T> {
 	public void removeByLast(){
 
 	}
-	
+
+	private MyDoubleLinkedNode<T> getLastNode(MyDoubleLinkedNode<T> node){
+
+		return node;
+	}
 }
