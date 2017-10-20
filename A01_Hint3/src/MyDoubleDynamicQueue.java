8eba3a33c5841e2ba66c875a6432a0625f3b096a
JohnnyB87 <johnbrady1987@gmail.com>
2017-10-20 12:19:51 +0100

    Implemented the addByFirst and addByLast methods

--- A01_Hint3/src/MyDoubleDynamicQueue.java
+++ A01_Hint3/src/MyDoubleDynamicQueue.java
@@ -6,6 +6,8 @@ public class MyDoubleDynamicQueue<T> implements MyQueue<T> {
 	//--------------------------------------------------
 	MyDoubleLinkedNode<T> head;
 	MyDoubleLinkedNode<T> tail;
+	MyDoubleLinkedNode<T> originalHead;
+	MyDoubleLinkedNode<T> originalTail;
 	int numItems;
 	int tailItems;
 	int headItems;
@@ -17,8 +19,8 @@ public class MyDoubleDynamicQueue<T> implements MyQueue<T> {
 		this.numItems = 0;
 		this.headItems = 0;
 		this.tailItems = 0;
-		this.head = null;
-		this.tail = null;
+		this.originalHead = this.head = null;
+		this.originalTail = this.tail = null;
 	}
 
 	//-------------------------------------------------------------------
@@ -32,6 +34,8 @@ public class MyDoubleDynamicQueue<T> implements MyQueue<T> {
 	// Basic Operation (Partial) --> Get first element from front of MyQueue: first
 	//-------------------------------------------------------------------
 	public T first(){
+		if(this.head == null)
+			return this.originalTail.getInfo();
 		return this.head.getInfo();
 	}
 
@@ -39,30 +43,39 @@ public class MyDoubleDynamicQueue<T> implements MyQueue<T> {
 	// Basic Operation --> Add element to back of MyQueue: addByFirst 
 	//-------------------------------------------------------------------
 	public void addByFirst(T element){
-		if(this.head == null && this.tail == null) {
-			this.head = new MyDoubleLinkedNode<T>(null, element, this.tail);
-			this.tail = new MyDoubleLinkedNode<T>(this.head, null, null);
+		if(this.head == null) {
+			this.head = new MyDoubleLinkedNode<T>(null, element, this.originalTail);
+			this.originalHead = this.head;
 		}
 		else {
 			MyDoubleLinkedNode<T> currentNode = this.head;
 			this.head = new MyDoubleLinkedNode<T>(null,element,currentNode);
 			currentNode.setLeft(this.head);
-
 		}
 		this.numItems++;
+		this.headItems++;
 	}
 	
 	//-------------------------------------------------------------------
 	// Basic Operation (Partial) --> Remove element from front of MyQueue: removeByFirst 
 	//-------------------------------------------------------------------	
 	public void removeByFirst(){
+		if(this.numItems == 0)
+			System.out.println("ERROR: list is empty.");
+		else if(this.head == null){
 
+		}
+		else{
+
+		}
 	}
 
 	//-------------------------------------------------------------------
 	// Basic Operation (Partial) --> Get first element from front of MyQueue: last
 	//-------------------------------------------------------------------
 	public T last(){
+		if(this.tail==null)
+			return this.originalHead.getInfo();
 		return this.tail.getInfo();
 	}
 
@@ -70,7 +83,17 @@ public class MyDoubleDynamicQueue<T> implements MyQueue<T> {
 	// Basic Operation --> Add element to back of MyQueue: addByLast 
 	//-------------------------------------------------------------------
 	public void addByLast(T element){
-
+		if(this.tail == null){
+			this.tail = new MyDoubleLinkedNode<T>(this.originalHead,element,null);
+			this.originalTail = this.tail;
+		}
+		else{
+			MyDoubleLinkedNode<T> currentNode = this.tail;
+			this.tail = new MyDoubleLinkedNode<T>(currentNode,element,null);
+			currentNode.setRight(this.tail);
+		}
+		this.numItems++;
+		this.tailItems++;
 	}
 	
 	//-------------------------------------------------------------------
